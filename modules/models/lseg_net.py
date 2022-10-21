1de9b0cdf662ee05c6fd17ccc4ce71f0ab7d1e89
Jiang, Donggang <donggang.jiang@intel.com>
2022-10-21 09:23:37 +0800

    Replace hook register with new forward pipeline call

--- modules/models/lseg_net.py
+++ modules/models/lseg_net.py
@@ -172,7 +172,7 @@ class LSeg(BaseModel):
         if self.channels_last == True:
             x.contiguous(memory_format=torch.channels_last)
 
-        layer_1, layer_2, layer_3, layer_4 = forward_vit(self.pretrained, x)
+        layer_1, layer_2, layer_3, layer_4 = self.forward_vit(x)
 
         layer_1_rn = self.scratch.layer1_rn(layer_1)
         layer_2_rn = self.scratch.layer2_rn(layer_2)
