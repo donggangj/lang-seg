45d345a403f1034f815d54f12ba7840745a407f2
Jiang, Donggang <donggang.jiang@intel.com>
2022-10-26 09:58:16 +0800

    Add an inference module of LSeg to remove dependency of torch-encoding

---
+++ modules/lseg_inference.py
@@ -0,0 +1,288 @@
+import os
+from argparse import ArgumentParser
+
+import pytorch_lightning as pl
+import torch
+# add mixed precision
+import torch.cuda.amp as amp
+import torchvision.transforms as transforms
+
+# from data import get_available_datasets
+from models.lseg_net import LSegNet
+
+
+class LSegModuleInference(pl.LightningModule):
+    def __init__(self, data_path, dataset, batch_size, base_lr, max_epochs, **kwargs):
+        super().__init__()
+
+        self.data_path = data_path
+        self.batch_size = batch_size
+        self.base_lr = base_lr / 16 * batch_size
+        self.lr = self.base_lr
+
+        self.epochs = max_epochs
+        self.other_kwargs = kwargs
+        self.enabled = False  # True mixed precision will make things complicated and leading to NAN error
+        self.scaler = amp.GradScaler(enabled=self.enabled)
+
+    def forward(self, x):
+        return self.net(x)
+
+    def configure_optimizers(self):
+        params_list = [
+            {"params": self.net.pretrained.parameters(), "lr": self.base_lr},
+        ]
+        if hasattr(self.net, "scratch"):
+            print("Found output scratch")
+            params_list.append(
+                {"params": self.net.scratch.parameters(), "lr": self.base_lr * 10}
+            )
+        if hasattr(self.net, "auxlayer"):
+            print("Found auxlayer")
+            params_list.append(
+                {"params": self.net.auxlayer.parameters(), "lr": self.base_lr * 10}
+            )
+        if hasattr(self.net, "scale_inv_conv"):
+            print(self.net.scale_inv_conv)
+            print("Found scaleinv layers")
+            params_list.append(
+                {
+                    "params": self.net.scale_inv_conv.parameters(),
+                    "lr": self.base_lr * 10,
+                }
+            )
+            params_list.append(
+                {"params": self.net.scale2_conv.parameters(), "lr": self.base_lr * 10}
+            )
+            params_list.append(
+                {"params": self.net.scale3_conv.parameters(), "lr": self.base_lr * 10}
+            )
+            params_list.append(
+                {"params": self.net.scale4_conv.parameters(), "lr": self.base_lr * 10}
+            )
+
+        if self.other_kwargs["midasproto"]:
+            print("Using midas optimization protocol")
+
+            opt = torch.optim.Adam(
+                params_list,
+                lr=self.base_lr,
+                betas=(0.9, 0.999),
+                weight_decay=self.other_kwargs["weight_decay"],
+            )
+            sch = torch.optim.lr_scheduler.LambdaLR(
+                opt, lambda x: pow(1.0 - x / self.epochs, 0.9)
+            )
+
+        else:
+            opt = torch.optim.SGD(
+                params_list,
+                lr=self.base_lr,
+                momentum=0.9,
+                weight_decay=self.other_kwargs["weight_decay"],
+            )
+            sch = torch.optim.lr_scheduler.LambdaLR(
+                opt, lambda x: pow(1.0 - x / self.epochs, 0.9)
+            )
+        return [opt], [sch]
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument(
+            "--data_path", type=str, help="path where dataset is stored"
+        )
+        parser.add_argument(
+            "--dataset",
+            # choices=get_available_datasets(),
+            default="ade20k",
+            help="dataset to train on",
+        )
+        parser.add_argument(
+            "--batch_size", type=int, default=16, help="size of the batches"
+        )
+        parser.add_argument(
+            "--base_lr", type=float, default=0.004, help="learning rate"
+        )
+        parser.add_argument("--momentum", type=float, default=0.9, help="SGD momentum")
+        parser.add_argument(
+            "--weight_decay", type=float, default=1e-4, help="weight_decay"
+        )
+        parser.add_argument(
+            "--aux", action="store_true", default=False, help="Auxilary Loss"
+        )
+        parser.add_argument(
+            "--aux-weight",
+            type=float,
+            default=0.2,
+            help="Auxilary loss weight (default: 0.2)",
+        )
+        parser.add_argument(
+            "--se-loss",
+            action="store_true",
+            default=False,
+            help="Semantic Encoding Loss SE-loss",
+        )
+        parser.add_argument(
+            "--se-weight", type=float, default=0.2, help="SE-loss weight (default: 0.2)"
+        )
+
+        parser.add_argument(
+            "--midasproto", action="store_true", default=False, help="midasprotocol"
+        )
+
+        parser.add_argument(
+            "--ignore_index",
+            type=int,
+            default=-1,
+            help="numeric value of ignore label in gt",
+        )
+        parser.add_argument(
+            "--augment",
+            action="store_true",
+            default=False,
+            help="Use extended augmentations",
+        )
+
+        return parser
+
+
+def get_labels(dataset):
+    labels = []
+    path = 'label_files/{}_objectInfo150.txt'.format(dataset)
+    assert os.path.exists(path), '*** Error : {} not exist !!!'.format(path)
+    f = open(path, 'r')
+    lines = f.readlines()
+    for line in lines:
+        label = line.strip().split(',')[-1].split(';')[0]
+        labels.append(label)
+    f.close()
+    if dataset in ['ade20k']:
+        labels = labels[1:]
+    return labels
+
+
+class LSegInference(LSegModuleInference):
+    def __init__(self, data_path, dataset, batch_size, base_lr, max_epochs, **kwargs):
+        super(LSegInference, self).__init__(
+            data_path, dataset, batch_size, base_lr, max_epochs, **kwargs
+        )
+
+        if dataset == "citys":
+            self.base_size = 2048
+            self.crop_size = 768
+        else:
+            self.base_size = 520
+            self.crop_size = 480
+
+        norm_mean = [0.5, 0.5, 0.5]
+        norm_std = [0.5, 0.5, 0.5]
+
+        print('** Use norm {}, {} as the mean and std **'.format(norm_mean, norm_std))
+
+        train_transform = [
+            transforms.ToTensor(),
+            transforms.Normalize(norm_mean, norm_std),
+        ]
+
+        val_transform = [
+            transforms.ToTensor(),
+            transforms.Normalize(norm_mean, norm_std),
+        ]
+
+        self.train_transform = transforms.Compose(train_transform)
+        self.val_transform = transforms.Compose(val_transform)
+        # print(kwargs)
+
+        labels = get_labels('ade20k')
+
+        self.net = LSegNet(
+            labels=labels,
+            backbone=kwargs["backbone"],
+            features=kwargs["num_features"],
+            crop_size=self.crop_size,
+            arch_option=kwargs["arch_option"],
+            block_depth=kwargs["block_depth"],
+            activation=kwargs["activation"],
+        )
+
+        self.net.pretrained.model.patch_embed.img_size = (
+            self.crop_size,
+            self.crop_size,
+        )
+
+        self.mean = norm_mean
+        self.std = norm_std
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = LSegModuleInference.add_model_specific_args(parent_parser)
+        parser = ArgumentParser(parents=[parser])
+
+        parser.add_argument(
+            "--backbone",
+            type=str,
+            default="clip_vitl16_384",
+            help="backbone network",
+        )
+
+        parser.add_argument(
+            "--num_features",
+            type=int,
+            default=256,
+            help="number of featurs that go from encoder to decoder",
+        )
+
+        parser.add_argument("--dropout", type=float, default=0.1, help="dropout rate")
+
+        parser.add_argument(
+            "--finetune_weights", type=str, help="load weights to finetune from"
+        )
+
+        parser.add_argument(
+            "--no-scaleinv",
+            default=True,
+            action="store_false",
+            help="turn off scaleinv layers",
+        )
+
+        parser.add_argument(
+            "--no-batchnorm",
+            default=False,
+            action="store_true",
+            help="turn off batchnorm",
+        )
+
+        parser.add_argument(
+            "--widehead", default=False, action="store_true", help="wider output head"
+        )
+
+        parser.add_argument(
+            "--widehead_hr",
+            default=False,
+            action="store_true",
+            help="wider output head",
+        )
+
+        parser.add_argument(
+            "--arch_option",
+            type=int,
+            default=0,
+            help="which kind of architecture to be used",
+        )
+
+        parser.add_argument(
+            "--block_depth",
+            type=int,
+            default=0,
+            help="how many blocks should be used",
+        )
+
+        parser.add_argument(
+            "--activation",
+            choices=['lrelu', 'tanh'],
+            default="lrelu",
+            help="use which activation to activate the block",
+        )
+
+        return parser
