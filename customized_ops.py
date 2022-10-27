e8869fe28132d7877961e4ef6ab6fcf4330a51fa
Jiang, Donggang <donggang.jiang@intel.com>
2022-10-27 10:10:26 +0800

    Add ResidualAttentionBlockCustom in customized_ops

--- customized_ops.py
+++ customized_ops.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from typing import Tuple, Optional
 
 import torch
@@ -78,6 +79,11 @@ class CLIP(nn.Module):
         return logits_per_image, logits_per_text
 
 
+class QuickGELU(nn.Module):
+    def forward(self, x: torch.Tensor):
+        return x * torch.sigmoid(1.702 * x)
+
+
 class MultiheadAttentionCustom(nn.Module):
     """
     Add to clip/model.py
@@ -147,5 +153,25 @@ class MultiheadAttentionCustom(nn.Module):
 
 
 class ResidualAttentionBlockCustom(nn.Module):
-    def __init__(self):
+    """
+    Add to clip/model.py
+    """
+    def __init__(self, d_model: int, n_head: int):
         super().__init__()
+
+        self.attn = MultiheadAttentionCustom(d_model, n_head)
+        self.ln_1 = LayerNorm(d_model)
+        self.mlp = nn.Sequential(OrderedDict([
+            ("c_fc", nn.Linear(d_model, d_model * 4)),
+            ("gelu", QuickGELU()),
+            ("c_proj", nn.Linear(d_model * 4, d_model))
+        ]))
+        self.ln_2 = LayerNorm(d_model)
+
+    def attention(self, x: torch.Tensor):
+        return self.attn(x, x, x)[0]
+
+    def forward(self, x: torch.Tensor):
+        x = x + self.attention(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
