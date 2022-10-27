1403d29611fdd179145f58617b9770d840b9450e
Jiang, Donggang <donggang.jiang@intel.com>
2022-10-27 16:29:31 +0800

    Add support for converting MultiheadAttentionCustom in convert_weights

--- customized_ops.py
+++ customized_ops.py
@@ -195,3 +195,56 @@ class TransformerCustom(nn.Module):
 
     def forward(self, x: torch.Tensor):
         return self.resblocks(x)
+
+
+def convert_weights(model: nn.Module):
+    """
+    From clip/model.py
+
+    ORIGINAL CODE:
+    =====================================================
+    def convert_weights(model: nn.Module):
+
+        def _convert_weights_to_fp16(l):
+            if isinstance(l, (nn.Conv1d, nn.Conv2d, nn.Linear)):
+                l.weight.data = l.weight.data.half()
+                if l.bias is not None:
+                    l.bias.data = l.bias.data.half()
+
+            if isinstance(l, nn.MultiheadAttention):
+                for attr in [*[f"{s}_proj_weight" for s in ["in", "q", "k", "v"]], "in_proj_bias", "bias_k", "bias_v"]:
+                    tensor = getattr(l, attr)
+                    if tensor is not None:
+                        tensor.data = tensor.data.half()
+
+            for name in ["text_projection", "proj"]:
+                if hasattr(l, name):
+                    attr = getattr(l, name)
+                    if attr is not None:
+                        attr.data = attr.data.half()
+
+        model.apply(_convert_weights_to_fp16)
+    =====================================================
+
+    Convert applicable model parameters to fp16
+    """
+
+    def _convert_weights_to_fp16(l):
+        if isinstance(l, (nn.Conv1d, nn.Conv2d, nn.Linear)):
+            l.weight.data = l.weight.data.half()
+            if l.bias is not None:
+                l.bias.data = l.bias.data.half()
+
+        if type(l) in (nn.MultiheadAttention, MultiheadAttentionCustom):
+            for attr in [*[f"{s}_proj_weight" for s in ["in", "q", "k", "v"]], "in_proj_bias", "bias_k", "bias_v"]:
+                tensor = getattr(l, attr)
+                if tensor is not None:
+                    tensor.data = tensor.data.half()
+
+        for name in ["text_projection", "proj"]:
+            if hasattr(l, name):
+                attr = getattr(l, name)
+                if attr is not None:
+                    attr.data = attr.data.half()
+
+    model.apply(_convert_weights_to_fp16)
