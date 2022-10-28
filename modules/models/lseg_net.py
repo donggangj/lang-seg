83de09325c6ae659bb8e4b71311cbee4b3896adc
Jiang, Donggang <donggang.jiang@intel.com>
2022-10-28 12:47:00 +0800

    Add TextEncoder to extract necessary component for LSeg

--- modules/models/lseg_net.py
+++ modules/models/lseg_net.py
@@ -117,6 +117,32 @@ def _make_fusion_block(features, use_bn):
     )
 
 
+class TextEncoder(nn.Module):
+    def __init__(self, clip_module):
+        super(TextEncoder, self).__init__()
+        self.dtype = clip_module.dtype
+        self.token_embedding = clip_module.token_embedding
+        self.positional_embedding = clip_module.positional_embedding
+        self.transformer = clip_module.transformer
+        self.ln_final = clip_module.ln_final
+        self.text_projection = clip_module.text_projection
+
+    def forward(self, text):
+        x = self.token_embedding(text).type(self.dtype)  # [batch_size, n_ctx, d_model]
+
+        x = x + self.positional_embedding.type(self.dtype)
+        x = x.permute(1, 0, 2)  # NLD -> LND
+        x = self.transformer(x)
+        x = x.permute(1, 0, 2)  # LND -> NLD
+        x = self.ln_final(x).type(self.dtype)
+
+        # x.shape = [batch_size, n_ctx, transformer.width]
+        # take features from the eot embedding (eot_token is the highest number in each sequence)
+        x = x[torch.arange(x.shape[0]), text.argmax(dim=-1)] @ self.text_projection
+
+        return x
+
+
 class LSeg(BaseModel):
     def __init__(
             self,
