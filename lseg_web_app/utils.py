9090c2ebaa476e5d57faa00eb4655b282a3d72c4
Jiang, Donggang <donggang.jiang@intel.com>
2022-12-13 09:57:28 +0800

    Add result_timeout_in_seconds into lseg habana app config json and utils

--- lseg_web_app/utils.py
+++ lseg_web_app/utils.py
@@ -97,6 +97,7 @@ def default_config():
             'output_key': 'output',
             'device_name_key': 'device_name',
             'sleep_seconds_for_io': 0.3,
+            'result_timeout_in_seconds': 60,
             'image_hw': [360, 480]}
 
 
