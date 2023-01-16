190b47b3018b6c8d900d3c528238dd3ab70a7348
Jiang, Donggang <donggang.jiang@intel.com>
2023-01-16 16:34:31 +0800

    Fix typo

--- lseg_web_app/utils.py
+++ lseg_web_app/utils.py
@@ -143,26 +143,26 @@ def calc_error(pred: np.ndarray, ref: np.ndarray):
     return mae, rmse
 
 
-def get_new_pallete(num_cls):
+def get_new_palette(num_cls):
     n = num_cls
-    pallete = [0] * (n * 3)
+    palette = [0] * (n * 3)
     for j in range(0, n):
         lab = j
-        pallete[j * 3 + 0] = 0
-        pallete[j * 3 + 1] = 0
-        pallete[j * 3 + 2] = 0
+        palette[j * 3 + 0] = 0
+        palette[j * 3 + 1] = 0
+        palette[j * 3 + 2] = 0
         i = 0
         while lab > 0:
-            pallete[j * 3 + 0] |= (((lab >> 0) & 1) << (7 - i))
-            pallete[j * 3 + 1] |= (((lab >> 1) & 1) << (7 - i))
-            pallete[j * 3 + 2] |= (((lab >> 2) & 1) << (7 - i))
+            palette[j * 3 + 0] |= (((lab >> 0) & 1) << (7 - i))
+            palette[j * 3 + 1] |= (((lab >> 1) & 1) << (7 - i))
+            palette[j * 3 + 2] |= (((lab >> 2) & 1) << (7 - i))
             i = i + 1
             lab >>= 3
-    return pallete
+    return palette
 
 
-def get_new_mask_pallete(npimg, new_palette, out_label_flag=False, labels=None):
-    """Get image color pallete for visualizing masks"""
+def get_new_mask_palette(npimg, new_palette, out_label_flag=False, labels=None):
+    """Get image color palette for visualizing masks"""
     # put colormap
     out_img = Image.fromarray(npimg.squeeze().astype('uint8'))
     out_img.putpalette(new_palette)
@@ -185,8 +185,8 @@ def get_new_mask_pallete(npimg, new_palette, out_label_flag=False, labels=None):
 def get_result_figure(image: Image.Image, labels: List[str], output: np.ndarray,
                       save_path='', title='', alpha=0.5):
     predict = np.argmax(output, 1)
-    new_palette = get_new_pallete(len(labels))
-    mask, patches = get_new_mask_pallete(predict, new_palette, out_label_flag=True, labels=labels)
+    new_palette = get_new_palette(len(labels))
+    mask, patches = get_new_mask_palette(predict, new_palette, out_label_flag=True, labels=labels)
     seg = mask.convert("RGBA")
     out = Image.blend(image, seg, alpha)
     fig = plt.figure(figsize=(19.2, 3.6))
