39fc33bc750bb289527beee0a19a5a5a5c12432e
Jiang, Donggang <donggang.jiang@intel.com>
2023-03-09 09:58:45 +0800

    Rename modules/ to lseg/

--- lseg_web_app/lseg_backend.py
+++ lseg_web_app/lseg_backend.py
@@ -15,7 +15,7 @@ from torchvision import transforms
 from lseg_web_app.models import LSeg_habana_MultiEvalModule
 from lseg_web_app.utils import Options, MD5Table, default_config
 from lseg_web_app.utils import load_config, check_dir
-from modules.lseg_inference import LSegInference
+from lseg.lseg_inference import LSegInference
 
 
 class BackendOptions(Options):
